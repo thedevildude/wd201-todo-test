d18287b29eeaea5e8a9a94cc24862b708949d837
Devdeep Ghosh <devarmforce01@gmail.com>
2022-10-03 05:16:50 +0530

    Added lint staged

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,34 +1,28 @@
+/* eslint-disable no-undef */
 const todoList = require("../index.js");
 
-const {add, all, markAsComplete} = todoList();
+const { add, all, markAsComplete } = todoList();
 
 describe("Todo Test Case", () => {
-
-    beforeAll(() => {
-        add 
-        (
-            {
-                title:"Test Title",
-                completed: false,
-                dueDate: new Date().toLocaleDateString("en-CA")
-            }
-        )
-    })
-    test("Should add new todo", () => {
-        const todoItemsCount = all.length
-        add 
-        (
-            {
-                title:"Test Title",
-                completed: false,
-                dueDate: new Date().toLocaleDateString("en-CA")
-            }
-        )
-        expect(all.length).toBe(todoItemsCount + 1)
-    })
-    test("Should mark a todo as complete", () => {
-        expect(all[0].completed).toBe(false)
-        markAsComplete(0)
-        expect(all[0].completed).toBe(true)
-    })
-})
+  beforeAll(() => {
+    add({
+      title: "Test Title",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+  });
+  test("Should add new todo", () => {
+    const todoItemsCount = all.length;
+    add({
+      title: "Test Title",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(todoItemsCount + 1);
+  });
+  test("Should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+});
