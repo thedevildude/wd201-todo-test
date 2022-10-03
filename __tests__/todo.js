cada3b341567cc1aa8e9c881e327b8a1f571db3a
Devdeep Ghosh <devarmforce01@gmail.com>
2022-10-03 05:45:52 +0530

    Added test cases

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,20 +1,38 @@
 /* eslint-disable no-undef */
 const todoList = require("../index.js");
 
-const { add, all, markAsComplete } = todoList();
+const { add, all, markAsComplete, overdue, dueToday, dueLater } = todoList();
 
-describe("Todo Test Case", () => {
+describe("Todo List Test Suite", () => {
   beforeAll(() => {
-    add({
-      title: "Test Title",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
-    });
+    const today = new Date();
+    const oneDay = 60 * 60 * 24 * 1000;
+    [
+      {
+        title: "Task 1",
+        completed: false,
+        dueDate: new Date(today.getTime() - 3 * oneDay).toLocaleDateString(
+          "en-CA"
+        ),
+      },
+      {
+        title: "Task 2",
+        completed: false,
+        dueDate: new Date().toLocaleDateString("en-CA"),
+      },
+      {
+        title: "Task 3",
+        completed: false,
+        dueDate: new Date(today.getTime() + 3 * oneDay).toLocaleDateString(
+          "en-CA"
+        ),
+      },
+    ].forEach(add);
   });
   test("Should add new todo", () => {
     const todoItemsCount = all.length;
     add({
-      title: "Test Title",
+      title: "Test 4",
       completed: false,
       dueDate: new Date().toLocaleDateString("en-CA"),
     });
@@ -24,5 +42,14 @@ describe("Todo Test Case", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+  test("Should retrieve overdue items", () => {
+    expect(overdue().length).toEqual(1);
+  });
+  test("Should retrieve due today items", () => {
+    expect(dueToday().length).toEqual(2);
+  });
+  test("Should retrieve due later items", () => {
+    expect(dueLater().length).toEqual(1);
   });
 });
